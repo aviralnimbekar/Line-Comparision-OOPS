87417e125bfc9a8e3cb09eb5571fee21780fa1ec
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-16 15:18:33 +0530

    ADDING : Comprarison of two lines program

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -12,10 +12,9 @@ public class LineComparison {
     /**
      * Purpose - Modeling of line
      *
-     * @param lineNum coordinate of lines
      * @return length of line
      */
-    public static double lineLength(int lineNum) {
+    public static double lineLength() {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter x1 point");
@@ -30,9 +29,19 @@ public class LineComparison {
         int X = x2 - x1;
         int Y = y2 - y1;
         double length = Math.sqrt((X * X) + (Y * Y));
-        System.out.println("Length of Line-" + lineNum + " = " + length + "unit");
 
         return length;
+    }
+
+    /**
+     * Purpose - shows equality of two line
+     */
+    public static void comparison(double lengthA, double lengthB) {
+
+        if (lengthA == lengthB)
+            System.out.println("Line1 is equal to Line2");
+        else
+            System.out.println("Line1 is not equal to Line2");
     }
 
     /**
@@ -42,6 +51,12 @@ public class LineComparison {
         System.out.println("Welcome to Line Comparison Program");
 
         System.out.println("Enter coordinate of line-1");
-        lineLength(1);
+        double length1 = lineLength();
+        System.out.println("Enter coordinate of line-2");
+        double length2 = lineLength();
+        System.out.println("Length of Line-1 = " + length1 + "\n"
+                + "Length of Line-2 = " + length2);
+
+        comparison(length1, length2);
     }
 }
